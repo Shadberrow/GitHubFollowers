86f311dd34f3beab27738bd9741d57f3952cd202
Yevhenii Veretennikov <veret.evg@gmail.com>
2020-09-23 17:03:25 +0300

    Initial

--- GHF/Utilities/GHFError.swift
+++ GHF/Utilities/GHFError.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+enum GHFError: String, Error {
+
+    case invalidUsername    = "This user name created an invalid request. Please try again."
+    case unableToComplete   = "Unable to complete your request. Please check yout Internet connection."
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from server was invalid. Please try again."
+
+    case unableToFavorite   = "There was an error favoriting this user. Please try again"
+    case alreadyInFavorites = "You've alredy favorited this user."
+
+}
+
